ca63bd015e468a759bfc3cb8e063a4f318b5b1c8
QuillX4 <advensicaddy@gmail.com>
2023-09-20 18:18:29 +0530

    some patterns

--- patterns.cpp
+++ patterns.cpp
@@ -13,9 +13,59 @@ void pattern1(int n){
 	}
 }
 
+void pattern2(int n){
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	
+}
+
+void pattern3(int n){
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i ; j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+	
+}
+
+void pattern4(int n){
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i ; j++)
+		{
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+	
+}
+
+void pattern5(int n){
+	for (int i = 0; i < n; i++)
+	{
+		// for (int j = 0; j < n-i+1 ; j++)
+		for (int j = n; j > i ; j--)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	
+}
+
+
 int main(){
 	int n;
 	cin>>n;
-	pattern1(n);
+	pattern5(n);
 	
 }
