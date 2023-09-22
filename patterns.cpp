34d43b312abc5327790e8439c5327e6067dd775a
QuillX4 <advensicaddy@gmail.com>
2023-09-22 19:23:22 +0530

    commit

--- patterns.cpp
+++ patterns.cpp
@@ -141,9 +141,49 @@ void pattern10(int n){
 	
 }
 
+void pattern11(int n){
+	int start;
+	for (int i = 0; i < n; i++)
+	{
+		if(i%2==0) start=1;
+		else start=0;
+		for (int j = 0; j <= i; j++)
+		{
+			cout<<start<<" ";
+			start=1-start;
+		}
+		cout<<endl;
+		
+	}
+	
+}
+
+void pattern12(int n){
+	int space = 2*(n-1);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout<<j;
+		}
+		for (int j = 0; j < space; j++)
+		{
+			cout<<" ";
+		}
+		for (int j = i; j >=1 ; j--)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+		space=space-2;
+		
+	}
+	
+}
+
 int main(){
 	int n;
 	cin>>n;
-	pattern10(n);
+	pattern12(n);
 	
 }
