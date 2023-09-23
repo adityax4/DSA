680e4024ecab9d89651ee1cb5e42d98eba548a42
QuillX4 <advensicaddy@gmail.com>
2023-09-23 19:48:11 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -181,9 +181,49 @@ void pattern12(int n){
 	
 }
 
+void pattern13(int n){
+   int start=1;
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j <= i; j++)
+      {
+         cout<<start<<" ";
+         start+=1;
+      }
+      cout<<endl;
+   }
+   
+}
+
+void pattern14(int n){
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (char ch = 'A'; ch <= 'A' + i; ch++)
+		{
+			cout<<ch<<" ";
+		}
+		cout<<endl;
+	}
+	
+}
+
+void pattern15(int n){
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (char ch = 'A'; ch <= 'A'+(n-i-1); ch++)
+		{
+			cout<<ch<<" ";
+		}
+		cout<<endl;
+	}
+	
+}
+
 int main(){
 	int n;
 	cin>>n;
-	pattern12(n);
+	pattern15(n);
 	
 }
---
+++ practice.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+   char ch, c;
+   ch='A';
+   c=ch+1;
+   cout<<c;
+}
