8bd9cf1beb4adc882fbde09c999ae5a03d7fb98b
QuillX4 <advensicaddy@gmail.com>
2023-09-24 18:32:19 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -221,9 +221,53 @@ void pattern15(int n){
 	
 }
 
+void pattern16(int n){
+	for (int i = 0; i < n; i++)
+	{
+		char ch = 'A' + i;
+		for (int j = 0; j <= i; j++)
+		{
+			cout<<ch<<" ";
+		}
+		cout<<endl;
+		
+	}
+	
+}
+
+void pattern17(int n){
+	for (int i = 0; i < n; i++)
+	{
+		//spaces
+		for (int j = 0; j < n-i-1; j++)
+		{
+			cout<<" ";
+		}
+		//alphabets
+		char ch = 'A';
+		int breakp = (2*i+1)/ 2;
+		for (int j = 1; j <= 2*i+1 ; j++)
+		{
+			cout<<ch;
+			if(j<=breakp){
+				ch++;
+			}
+			else ch--;
+		}
+		//spaces
+		for (int j = 0; j < n-i-1; j++)
+		{
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+	
+
+}
+
 int main(){
 	int n;
 	cin>>n;
-	pattern15(n);
+	pattern17(n);
 	
 }
