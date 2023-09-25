09956eeea8ec530681a78c607fcc5aedc05f3770
QuillX4 <advensicaddy@gmail.com>
2023-09-25 19:01:34 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -340,9 +340,24 @@ void pattern20(int n){
 	
 }
 
+void pattern21(int n){
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if(i==0 || i==n-1 || j==0 || j==n-1){
+				cout<<"*"<<" ";
+			}
+			else cout<<" "<<" ";
+		}
+		cout<<endl;
+	}
+	
+}
+
 int main(){
 	int n;
 	cin>>n;
-	pattern20(n);
+	pattern21(n);
 	
 }
