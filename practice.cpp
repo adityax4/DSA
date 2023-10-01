ffac4591bfb04afee1094e2fdc4debabbb57c65c
QuillX4 <advensicaddy@gmail.com>
2023-10-01 13:47:12 +0530

    learning recursion

--- practice.cpp
+++ practice.cpp
@@ -1,20 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+void printfun(int i, int n){
+   if(i>n) return;
+   printfun(i+1, n);
+   cout<<i<<" ";
+}
 
 int main(){
-   string str = "hello world";
-   for (int i = 0; i < str.length(); i++)
-   {
-      if(int(str[0])>96){
-         str[0] = (int)str[0] - 32;
-      }
-      if((int)str[i]==32){
-         
-      }
-      
-   }
-   
-   cout<<str;
+   int n;
+   cin>>n;
+   printfun(n,n);
 }
