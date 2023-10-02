4f7fea45d4429d077f4d41280016d62520169fc8
QuillX4 <advensicaddy@gmail.com>
2023-10-02 19:29:23 +0530

    fibonacci recursion

--- practice.cpp
+++ practice.cpp
@@ -1,18 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool func(string s, int i, int n){
-   if(i>=n){
-      return true;
+int fibo(int n){
+   if(n<=1){
+      return n;
    }
-   if(s[i]!=s[n-i-1]){
-      return false;
-   }
-   return func(s, i+1, n);
+   return fibo(n-1) + fibo(n-2);
 }
 
 int main(){
-   string s;
-   cin>>s;
-   cout<<func(s, 0, s.length());
+   int n;
+   cin>>n;
+   cout<<fibo(n);
 }
