306c327aab58a053f8524f218a11fb11274aca2e
QuillX4 <advensicaddy@gmail.com>
2023-10-03 19:41:30 +0530

    learning hashing

--- practice.cpp
+++ practice.cpp
@@ -1,10 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// map stores in sorted order
+
 int main(){
-   string name;
-   cin>>name;
-   for(int i=1; i<=10; i++){
-      cout<<i<<" "<<name<<endl;
+   int n;
+   cin>>n;
+   int arr[n]={};
+   for (int i = 0; i < n; i++)
+   {
+      cin>>arr[i];
+   }
+   
+   // pre computation
+   map<int, int> mpp;
+   for (int i = 0; i < n; i++)
+   {
+      mpp[arr[i]]++;
+   }
+   
+   // // iterate in the map
+   // for(auto it: mpp){
+   //    cout<<it.first<<"->"<<it.second<<endl;
+   // }
+
+   int m;
+   cin>>m;
+   while(m--){
+      int num;
+      cin>>num;
+      // fetch
+      cout<<mpp[num]<<endl;
    }
 }
