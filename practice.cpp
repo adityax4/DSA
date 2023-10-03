3c88cae97baa9726a8f24d417b0c9715804ecfef
QuillX4 <advensicaddy@gmail.com>
2023-10-03 17:20:34 +0530

    vscode setup

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibo(int n){
-   if(n<=1){
-      return n;
-   }
-   return fibo(n-1) + fibo(n-2);
-}
-
 int main(){
-   int n;
-   cin>>n;
-   cout<<fibo(n);
+   string name;
+   cin>>name;
+   for(int i=1; i<=10; i++){
+      cout<<i<<" "<<name<<endl;
+   }
 }
